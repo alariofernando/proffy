d7ab69a180f3a8979afc6c51246897feb591d2c7
FERNANDO ALARIO PIRES <servicoiteris738113@userede.com.br>
2020-08-08 19:06:24 -0300

    Select, textarea and TeacherList and Form

--- src/pages/teacherForm/index.tsx
+++ src/pages/teacherForm/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PageHeader from '../../components/page-header';
+import InputBlock from '../../components/inputs';
+import TextAreaBlock from '../../components/textarea';
+import SelectBlock from '../../components/select';
 
 import './styles.css';
-import InputBlock from '../../components/inputs';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([
+        {week_day:0, to:'', from:''}
+    ])
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            {
+                week_day: 0,
+                to:'',
+                from:''
+            }
+        ]);
+    }
+
     return (
     <div id="page-teacher-form" className="container">
         <PageHeader title='Que incrivel que você quer dar aulas.'
@@ -15,7 +35,66 @@ function TeacherForm() {
                 <InputBlock label="Nome Completo" name="name"/>
                 <InputBlock label="URL do seu avatar" name="avatar"/>
                 <InputBlock label="Seu número de whatsapp" name="whatsapp"/>
+                <TextAreaBlock label="Sua biografia" name="bio" />
             </fieldset>
+            <fieldset>
+                <legend>Sobre a aula</legend>
+                <SelectBlock label="Matéria"
+                 name="subject"
+                 options= {[
+                     {label: "Artes", value: "artes"},
+                     {label: "Matemática", value: "matematica"},
+                     {label: "Música", value: "musica"},
+                     {label: "Português", value: "portugues"},
+                     {label: "Geografia", value: "geografia"},
+                     {label: "História", value: "historia"},
+                     {label: "Filosofia", value: "filosofia"},
+
+                 ]}/>
+                <InputBlock label="Custa da aula por hora" name="cost"/>
+            </fieldset>
+
+            <fieldset>
+                <legend>
+                    Horários Disponívels
+                    <button type="button" onClick={addNewScheduleItem}>
+                        + Novo horário
+                    </button>
+                </legend>
+
+                {scheduleItems.map(scheduleItem => {
+                    return (
+                        <div key={scheduleItem.week_day} className="schedule-item">
+                            <SelectBlock label="Dia da semana"
+                            name="week_day"
+                            options={[
+                                {label:"Domingo", value: '0'},
+                                {label:"Segunda-feira", value: '1'},
+                                {label:"Terça-feira", value: '2'},
+                                {label:"Quarta-feira", value: '3'},
+                                {label:"Quinta-feira", value: '4'},
+                                {label:"Sexta-feira", value: '5'},
+                                {label:"Sábado", value: '6'},
+                                
+                            ]}/>
+                            <InputBlock name="from" label="Das" type="time" />
+                            <InputBlock name="to" label="Até" type="time" />
+                        </div>
+                    );                    
+                })}
+            </fieldset>
+
+            <footer>
+                <p>
+                    <img src={warningIcon} alt="Aviso importante"/>
+                    Importante! <br />
+                    Preencha todos os dados
+                </p>
+                <button>
+                    Salvar cadastro
+                </button>
+            </footer>
+
         </main>
     </div>
     );
--- src/pages/teacherList/index.tsx
+++ src/pages/teacherList/index.tsx
@@ -3,15 +3,38 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/page-header';
 import TeacherItem from '../../components/teacher-item';
-import InputBlock from '../../components/inputs'
+import InputBlock from '../../components/inputs';
+import SelectBlock from '../../components/select';
 
 function TeacherList() {
     return (
     <div id="page-teacher-list" className="container">
         <PageHeader title='Estes são os Proffys disponíveis'>
             <form id="search-teachers">
-                <InputBlock label="Matéria" name="subject"/>
-                <InputBlock label="Dia da semana" name="week_day"/>
+                <SelectBlock label="Matéria"
+                 name="subject"
+                 options= {[
+                     {label: "Artes", value: "artes"},
+                     {label: "Matemática", value: "matematica"},
+                     {label: "Música", value: "musica"},
+                     {label: "Português", value: "portugues"},
+                     {label: "Geografia", value: "geografia"},
+                     {label: "História", value: "historia"},
+                     {label: "Filosofia", value: "filosofia"},
+
+                 ]}/>
+                <SelectBlock label="Dia da semana"
+                 name="week_day"
+                 options={[
+                    {label:"Domingo", value: '0'},
+                    {label:"Segunda-feira", value: '1'},
+                    {label:"Terça-feira", value: '2'},
+                    {label:"Quarta-feira", value: '3'},
+                    {label:"Quinta-feira", value: '4'},
+                    {label:"Sexta-feira", value: '5'},
+                    {label:"Sábado", value: '6'},
+                    
+                 ]}/>
                 <InputBlock label="Hora" name="time" type="time"/>
                 
             </form>
